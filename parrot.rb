55faa5c5ba62e32352832abde58ae39b27c7a218
Ben C <bcwong1@ualberta.ca>
2016-01-23 20:40:42 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-def parrot(argument = "Squawk!")
-	puts argument
-	argument
+def parrot(string = "Squawk!")
+  puts "#{string}"
+  string
 end
 
 
