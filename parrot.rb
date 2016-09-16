1b7eb9613750b1873cdca44cab1e4e5d0a08246f
Scott Wakeman <swakeman@ca.rr.com>
2016-09-16 19:58:53 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,24 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+#parrot should output "Squawk!" to the terminal when called without any argument
+#Squawk! should output "Squawk!" to the terminal when called without any argument
 def parrot(string="Squawk!")
+  #Squawk! should output the given phrase when called with an argument
    puts "#{string}"
+   #Pretty bird! should return the given phrase when called with an argument
    "#{string}"
  end
-
-#def parrot(phrase="Squawk")
-  #puts "#{parrot}"
-  #puts "Squawk!"
-   #{}"#{parrot}"
-  #puts "#{parrot("Squawk!)}"
-  #(phrase="Pretty bird!")
-  #puts "#{phrase}"
-  #puts "Pretty bird!"
-  #{}"#{phrase}"
-  #puts "#{parrot(phrase)}"
-#end
-
-#should output "Squawk!" to the terminal when called without any argument
-#should return the default phrase, "Squawk!" when called without any arguments
-#should output the given phrase when called with an argument
-#should return the given phrase when called with an argument
