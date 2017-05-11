3c994214b7938bd57296e355aba0b0fe0a2aed23
Pat  Pignataro <pat6284@msn.com>
2017-05-11 15:23:24 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (string="Squawk")
-puts "#{string}"
-   "#{string}"
+def parrot (chirp ="Squawk!")
+puts chirp
+  return chirp
 end
- 
