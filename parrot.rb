402bc1ec6ed8f24395d405022a2804d3f6a7bcc1
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-06-21 16:14:32 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
+end
+
+#SET the value of a variable
+#GET the value of a variable
+#DEFINE a method
+#EXECUTE a method
