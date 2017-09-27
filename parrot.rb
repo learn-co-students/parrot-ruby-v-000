39dc0c5aae69545ea1574a3d559665ac61eba399
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-27 06:48:17 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+#In this lab you'll be defining a method called #parrot.
+#The #parrot method should accept an argument of a string and both #puts out that string and return the string at the end of the method.
+#The #parrot method should have a default argument of "Squawk!".
+
+def parrot(phrase="Squawk!")
+  puts phrase
+  return phrase
+end
