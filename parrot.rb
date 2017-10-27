1a6303ac49d4e386940f5c16cd0a5dde516c6d12
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-27 18:43:20 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot()
+  puts "Squawk!"
+  return "Squawk!"
+end
+
+# def restaurant
+#   restaurant_name = "Guy's American Kitchen & Bar"
+#   cuisine = "american"
+#   motto = "Welcome to Flavor Town!"
+# end
