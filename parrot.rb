3f78901d869c1f22b974feedcf548f5489a375b7
Ethel C <cofresi.ethel@gmail.com>
2018-03-20 03:42:51 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (input_phrase = "Squawk!")
+  puts input_phrase
+  return input_phrase
+end
+parrot("Hello")
