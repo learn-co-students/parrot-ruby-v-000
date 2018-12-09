dc07da77f0e4b6751d33e6b6cb1fb4bf10b6bd86
Erica Murdock-Waters <ericahpa@gmail.com>
2018-12-09 19:47:44 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
-def parrot(phrase = nil)
-  if phrase != nil
-    puts "Pretty bird!"   # outputs given phrase when #parrot is called w/an argument &
-    "Pretty bird!"        # returns the given phrase
-  else                    # when #parrot called w/ no arguments
-    puts "Squawk!"        # outputs "Squawk!" to terminal &
-    "Squawk!"             # returns default phrase
-  end
+def parrot(phrase="Squawk!")
+  puts phrase
+  phrase
 end
