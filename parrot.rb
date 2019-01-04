b541007a389de124d661be0d9b7f808220740ff1
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 23:41:14 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# method should accept an argument of a string and both `#puts` out that string *and* return the string at the end of the method.
+
+
+
+def parrot(name ="Squawk!")
+puts "#{name}"
+return "#{name}"
+end
+
+def parrot(am = "Squawk!")
+puts "#{am}"
+return "#{am}"
+end
--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,23 +1,23 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
-puts describe '#parrot' do
+ describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
     expect($stdout).to receive(:puts).with("Squawk!")
     parrot
   end
 
-  puts it 'should return the default phrase, "Squawk!" when called without any arguments' do
+ it 'should return the default phrase, "Squawk!" when called without any arguments' do
     phrase = parrot
     expect(phrase).to eq("Squawk!")
   end
 
-  puts it 'should output the given phrase when called with an argument' do
+   it 'should output the given phrase when called with an argument' do
     expect($stdout).to receive(:puts).with("Pretty bird!")
     parrot("Pretty bird!")
   end
 
-  puts it 'should return the given phrase when called with an argument' do
+ it 'should return the given phrase when called with an argument' do
     phrase = parrot("Pretty bird!")
 
     expect(phrase).to eq("Pretty bird!")
