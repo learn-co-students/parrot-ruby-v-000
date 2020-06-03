126177755aa115f46c7095c3323e2dd020a4620f
langan.kg Langan <langan.kg@gmail.com>
2020-06-03 11:51:36 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+  return phrase
+end
+
+parrot("Pretty Bird!")
+
+#def greeting(name, language="Ruby")
+  #puts "Hello, #{name}. We heard you are a great #{language} programmer."
+#end
+#def greeting(name = "Ruby programmer")
+  #puts "Hello, #{name}"
+#end
